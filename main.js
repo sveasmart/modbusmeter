8ef3baa8f2b8b0fc63501f862e6bed8eae7faa8e
Henrik Kniberg <h@kniberg.com>
2017-01-14 06:28:24 +0100

    Restored the tick functionality, now that helloworld has been verified to work through resin.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello smartgrid-meter")
+console.log("Starting up smartgrid-meter...");
+
+var Gpio = require('onoff').Gpio,
+  button = new Gpio(18, 'in', 'both');
+
+function tick() {
+  console.log("tick!")
+}
+
+button.watch(function(err, value) {
+  tick();
+});
+
+console.log("Cool, smartgrid-meter is running!");
+
