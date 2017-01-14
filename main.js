f5b9f0557d07096698631173e5a74c35d3adb271
Henrik Kniberg <h@kniberg.com>
2017-01-14 06:24:23 +0100

    turned it into a helloworld for testing purposes

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-console.log("Starting up smartgrid-meter...");
-
-var Gpio = require('onoff').Gpio,
-  button = new Gpio(18, 'in', 'both');
-
-function tick() {
-  console.log("tick!")
-}
-
-button.watch(function(err, value) {
-  tick();
-});
-
-console.log("Cool, smartgrid-meter is running!");
-
+console.log("Hello smartgrid-meter")
