1ac2f0e7b9a898a6177ccc0e882c1ebf35023cea
Henrik Kniberg <h@kniberg.com>
2017-01-21 11:36:03 +0100

    Now update.sh actually does what it is supposed to do :)

--- update.sh
+++ update.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+# This script will copy this version of Meter to the
+# $apps_root/meter directory.
+# It is intended to be used by the Updater.
+# https://github.com/sveasmart/updater
+#
 
-echo "OK, pretending to install Meter!"
-echo "apps_root: " $apps_root
+echo "Copying Meter to "$apps_root"/meter..."
+
+mkdir -p $apps_root/meter
+
+rm -rf $apps_root/src
+rm -rf $apps_root/test
+
+cp -R config $apps_root/meter/
+cp -R src $apps_root/meter/
+cp -R test $apps_root/meter/
+cp README.md $apps_root/meter/
+cp package.json $apps_root/meter/
+
+echo "Done! Meter successfully updated!"
+
