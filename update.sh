b3ba3e48c6d3c805582643923402623a17c86d5e
Henrik Kniberg <h@kniberg.com>
2017-01-21 13:54:08 +0100

    Fixed a path

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@
 # https://github.com/sveasmart/updater
 #
 
-echo "Copying Meter to "$apps_root"/meter..."
+echo "Copying Meter from "$update_root" to "$apps_root"/meter..."
 ls .
 
 mkdir -p $apps_root/meter
@@ -13,15 +13,15 @@ mkdir -p $apps_root/meter
 rm -rf $apps_root/src
 rm -rf $apps_root/test
 
-cp -R ./config $apps_root/meter/
-cp -R ./src $apps_root/meter/
-cp -R ./test $apps_root/meter/
-cp ./README.md $apps_root/meter/
-cp ./package.json $apps_root/meter/
-cp ./install/start.sh $apps_root/meter/
+cp -R $update_root/config $apps_root/meter/
+cp -R $update_root/src $apps_root/meter/
+cp -R $update_root/test $apps_root/meter/
+cp $update_root/README.md $apps_root/meter/
+cp $update_root/package.json $apps_root/meter/
+cp $update_root/install/start.sh $apps_root/meter/
 
 echo "Installing/restarting the systemd service..."
-cp ./install/meter.service /lib/systemd/system
+cp $update_root/install/meter.service /lib/systemd/system
 systemctl enable meter.service
 systemctl restart meter.service
 
