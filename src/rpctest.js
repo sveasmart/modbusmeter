774eaaf120b562244557585057065c8830e6e2b7
Henrik Kniberg <h@kniberg.com>
2017-06-02 12:34:23 +0200

    Updated the display RPC test

--- src/rpctest.js
+++ src/rpctest.js
@@ -2,14 +2,9 @@ var rpc = require('json-rpc2');
 
 var client = rpc.Client.$create(7890, 'localhost');
 
-// Call add function on the server
-
-client.call('add', [1,2], function(err, result) {
-  console.log('1 + 2 = ' + result);
-});
-
 client.call('writeText', ['hi!'], function(err, result) {
-  console.log("called writeText")
-  console.log("err: ", err)
-  console.log("result: " + result)
+  if (err) {
+    throw err
+  }
+  console.log("Successfully called writeText!")
 })
