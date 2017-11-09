097454527dc2fe95bb86fab398bc38512fa7694d
Henrik Kniberg <h@kniberg.com>
2017-11-09 13:22:39 +0700

    Refactored PulseDetector into a separate class

---
+++ src/pulse_detector.js
@@ -0,0 +1,77 @@
+const RpioClickDetector = require('./rpio_click_detector')
+const FakeClickDetector = require('./fake_click_detector')
+const PersistentCounter = require('./persistent_counter')
+
+let config = require('./meter-config').loadConfig()
+const fs = require('fs')
+const path = require('path')
+
+
+/**
+ * My job is to listen for pulses on an RPIO port, and
+ * write them to an inbox file, and increment a counter file.
+ */
+class PulseDetector {
+  constructor(meterName, pulseInputPin, dataDir, simulate, logPulseDetection, verboseLogging) {
+    this.meterName = meterName
+    this.pulseInputPin = pulseInputPin
+    this.dataDir = dataDir
+    this.simulate = simulate
+    this.logPulseDetection = logPulseDetection
+    this.verboseLogging = verboseLogging
+
+    this.inboxFile = path.join(dataDir, "inbox")
+    this.counterFile = path.join(dataDir, "counter")
+    this.pulseCounter = new PersistentCounter(this.counterFile)
+
+    console.log("Meter #" + this.meterName + " will receive ticks on pin " + this.pulseInputPin + " and store in " + this.inboxFile)
+
+    if (RpioClickDetector.hasRpio()) {
+      console.log("RPIO detected. Will listen for clicks on pin " + this.pulseInputPin)
+      this.clickDetector = new RpioClickDetector(this.pulseInputPin, this.verboseLogging)
+    } else {
+      console.log("No RPIO detected. Can't detect clicks.")
+      this.clickDetector = new FakeClickDetector()
+    }
+  }
+
+  start() {
+    this.clickDetector.setClickListener(() => {
+      this._registerPulse()
+    })
+    console.log("Pulse detection for meter " + this.meterName + " is now running!")
+    console.log(" - verboseLogging = " + this.verboseLogging)
+    console.log(" - logPulseDetection = " + this.logPulseDetection)
+    console.log(" - simulate = " + this.simulate)
+
+    if (this.simulate > 0) {
+      console.log("Meter " + this.meterName + " will register a simulated pulse every " + this.simulate + " seconds.")
+      setInterval(() => {
+        console.log("Simulating a pulse for meter " + this.meterName)
+        this._registerPulse()
+      }, this.simulate * 1000)
+    }
+
+  }
+
+  _registerPulse() {
+    let pulse = new Date().toISOString();
+    this._addPulseToInbox(pulse)
+    if (this.logPulseDetection) {
+      console.log("Detected pulse and stored it: " + pulse)
+    }
+  }
+
+  _addPulseToInbox(pulse) {
+    fs.appendFile(this.inboxFile, pulse + "\n", function(err) {
+      if (err) {
+        console.log("Something went wrong when adding a pulse to inbox", pulse, err)
+      }
+    })
+    this.pulseCounter.increment()
+  }
+
+}
+
+
+module.exports = PulseDetector
--- src/start_pulse_detector.js
+++ src/start_pulse_detector.js
@@ -1,64 +1,15 @@
-const RpioClickDetector = require('./rpio_click_detector')
-const FakeClickDetector = require('./fake_click_detector')
-const PersistentCounter = require('./persistent_counter')
-
 let config = require('./meter-config').loadConfig()
-const fs = require('fs')
-const path = require('path')
-const tickInputPin = config.tickInputPin
-const dataDir = config.dataDir
-const inboxFile = path.join(dataDir, "inbox")
-const counterFile = path.join(dataDir, "counter")
-const pulseCounter = new PersistentCounter(counterFile)
+const PulseDetector = require('./pulse_detector')
 
-const simulate = config.simulate
-const logPulseDetection = config.logPulseDetection
-const verboseLogging = config.verboseLogging
+const pulseDetector = new PulseDetector(
+  config.meterName,
+  config.tickInputPin,
+  config.dataDir,
+  config.simulate,
+  config.logPulseDetection,
+  config.verboseLogging
+)
 
-
-console.log("I receive ticks on pin " + tickInputPin)
-
-let clickDetector
-if (RpioClickDetector.hasRpio()) {
-  console.log("RPIO detected. Will listen for clicks on pin " + tickInputPin)
-  clickDetector = new RpioClickDetector(tickInputPin, verboseLogging)
-} else {
-  console.log("No RPIO detected. Can't detect clicks.")
-  clickDetector = new FakeClickDetector()
-}
-
-clickDetector.setClickListener(registerPulse)
-console.log("Pulse detection is now running!")
-console.log(" - verboseLogging = " + verboseLogging)
-console.log(" - logPulseDetection = " + logPulseDetection)
-console.log(" - simulate = " + simulate)
-
-/**
- * Saves this tick in 'pending'.
- * Will be sent to the server next time some other node program checks the file.
- */
-function registerPulse() {
-  let pulse = new Date().toISOString();
-  addPulseToInbox(pulse)
-  if (logPulseDetection) {
-    console.log("Detected pulse and stored it: " + pulse)
-  }
-}
-
-function addPulseToInbox(pulse) {
-  fs.appendFile(inboxFile, pulse + "\n", function(err) {
-    if (err) {
-      console.log("Something went wrong when adding a pulse to inbox", pulse, err)
-    }
-  })
-  pulseCounter.increment()
-}
+pulseDetector.start()
 
 
-if (simulate > 0) {
-  console.log("I will register a simulated tick every " + simulate + " seconds.")
-  setInterval(function() {
-    console.log("Simulating a tick")
-    registerPulse()
-  }, simulate * 1000)
-}
