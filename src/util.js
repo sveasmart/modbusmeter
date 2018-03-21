0aa9a6aafaf437157ab40a8feb22c3f7c2526462
Henrik Kniberg <h@kniberg.com>
2018-03-21 06:51:26 +0100

    tweaked logging

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 module.exports.displayMeasurements = function(measurements) {
   let result = ""
   measurements.forEach((measurement) => {
-    result += (measurement.serialNumber + ": " + measurement.energy + " Wh\n")
+    result += ("  meter #" + measurement.serialNumber + " => " + measurement.energy + " Wh\n")
   })
   return result
 }
