a992d4ac40070bb02f583335897972583d984b1e
jornik <jorgen.niko@gmail.com>
2021-02-02 20:39:41 +0100

    attempt to get version, just calling function which does a modbus call...

--- src/modbus_client.js
+++ src/modbus_client.js
@@ -78,7 +78,7 @@ class ModbusClient {
 
     console.log("readVersion called")
 
-    const register = 11 + 260
+    const register = 11 + 200
     const multiplyEnergyBy = this.multiplyEnergyBy
     let numberOfRegistersForMeterValueXXXX = 4
 
