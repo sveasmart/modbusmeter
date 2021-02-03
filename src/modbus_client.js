42d9234606b5cbb95188eb876942b40a70b8a758
jornik <jorgen.niko@gmail.com>
2021-02-03 23:40:31 +0100

    Getting version and vendor for first meter.

--- src/modbus_client.js
+++ src/modbus_client.js
@@ -109,7 +109,7 @@ class ModbusClient {
     console.log("deviceVersionResponse: ", deviceVersionResponse)
     console.log("deviceVersionResponse: ", JSON.stringify(deviceVersionResponse))
     // console.log("deviceVersion: " + deviceVersionResponse.payload.readIntBE(0, 1))
-    console.log("deviceVersion: " + deviceVersionResponse.payload.data[0])
+    console.log("deviceVersion: " + JSON.parse(deviceVersionResponse.toString()).payload.data[0])
     console.log('--------------------------------------------')
 
     // console.log(manufacturers[manu].registerOffsetPerMeterByVersion['' + deviceVersionResponse.payload.data[0]])
